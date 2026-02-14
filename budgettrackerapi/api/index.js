77a5dc13824a1ec959b884152559802a244c5886
Renji-Gall <gallrenco@gmail.com>
2026-02-14 00:16:32 -0500

    reworked for render

--- budgettrackerapi/api/index.js
+++ budgettrackerapi/api/index.js
@@ -147,8 +147,9 @@ app.post('/api/login', async (req, res) => {
   res.json({ token }); // ✅ always JSON
 });
 
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
